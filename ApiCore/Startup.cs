72c708b629694c8b4c1f61a5e53944fe9c305aee
seanfdonato <seanfdonato@gmail.com>
2019-04-01 23:51:23 -0300

    Swagger running

--- ApiCore/Startup.cs
+++ ApiCore/Startup.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-
+using Swashbuckle.AspNetCore.Swagger;
 namespace ApiCore
 {
     public class Startup
@@ -18,6 +18,7 @@ namespace ApiCore
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+
         }
 
         public IConfiguration Configuration { get; }
@@ -26,6 +27,11 @@ namespace ApiCore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +48,17 @@ namespace ApiCore
 
             app.UseHttpsRedirection();
             app.UseMvc();
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                
+            });
+
         }
     }
 }
---
+++ Infra/Context/Context.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+using Microsoft.Extensions.Configuration;
+
+namespace Infra.Context
+{
+    public class Context : IDisposable
+    {
+        public SqlConnection Connection { get; set; }
+
+        public Context(IConfiguration configuration)
+        {
+            //Connection = new SqlConnection(_configuration["StringConection"]);
+            //Connection.Open();
+        }
+        public void Dispose()
+        {
+            if (Connection.State != ConnectionState.Closed)
+            {
+                Connection.Close();
+            }
+
+        }
+    }
+}
